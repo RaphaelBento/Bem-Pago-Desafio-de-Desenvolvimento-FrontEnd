febf7a372ac7011c08be2eeb1319e116025e8f03
RaphaelBento <rafhaesilvalbento@gmail.com>
2022-07-13 09:25:14 -0300

    Primeiro commit via GitBash

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <Header></Header>
+
+
+  </div>
+  
+</template>
+
+<script>
+
+import Header from './components/Header.vue'
+
+export default {
+  components: {
+    Header
+  }
+  
+}
+</script>
+
+<style>
+#app {
+border-radius: 5px;
+}
+
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,37 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<template>
+  <div>
+ 
+  <div id="CardDemo">
+    <img src="https://img.icons8.com/pastel-glyph/64/000000/cardboard-box.png" alt="">
+<p id="TxtHeader">Formulário para compra de</p>
+  </div>
+  
+ </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+        nome:'Vaiavel nome'
+              }
+          }
+}
+</script>
+
+<style>
+#CardDemo{
+width: 30%;
+    background-color: #000080;
+    border-radius: 5px;
+    position: relative;
+    left: 30%;
+    margin-top: 10px;
+        border-bottom-left-radius: 345px 37px;
+    border-bottom-right-radius: 155px 85px;
+}
+#TxtHeader{
+    color: white;
+}
+</style>
